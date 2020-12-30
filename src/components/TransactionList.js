eda60939ad185eab46873fd65adfb372a69ebc45
konf1nement <clearddcreations@gmail.com>
2020-12-30 16:38:36 -0500

    components created

---
+++ src/components/TransactionList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const TransactionList = () => {
+    return (
+        <div>
+            <h3>History</h3>
+            <ul className="list">
+                <li className="minus">
+                    Cash <span>$400</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
