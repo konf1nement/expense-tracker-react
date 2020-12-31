6410473323e25a820c742bce3e747d11d4a883e1
konf1nement <clearddcreations@gmail.com>
2020-12-31 02:36:20 -0500

    done

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -4,18 +4,19 @@ import {GlobalContext} from '../context/GlobalState';
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0)
+    const clearForm = document.querySelector('.clearForm');
 
     const {addTransaction} = useContext(GlobalContext);
     const onSubmit = e => {
         e.preventDefault();
 
-        const newTransaction = {
-            id: Math.floor(Math.random() * 10000000),
-            text,
-            amount: +amount
-        }
+    const newTransaction = {
+        id: Math.floor(Math.random() * 10000000),
+        text,
+        amount: +amount
+    }
 
-        addTransaction(newTransaction);
+    addTransaction(newTransaction);
     }
 
     return (
@@ -24,13 +25,13 @@ export const AddTransaction = () => {
             <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" value={text} onChange={(e) => setText(e.target .value)} placeholder="Enter text..."/>
+                    <input className='clearForm' type="text" value={text} onChange={(e) => setText(e.target .value)} placeholder="Enter text..."/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">Amount
                     <br/>
                     (negative - expense, positive - income</label>
-                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..."/>
+                    <input className='clearForm' type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..."/>
                 </div>
                 <button className="btn" >Add Transaction</button>
             </form>
